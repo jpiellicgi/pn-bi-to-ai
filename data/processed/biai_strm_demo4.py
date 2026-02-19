46cc3c5421ed201a5e6dfda1bc74210b00280146
emily-wilkins-owen <emily.wilkins@cgi.com>
2026-02-19 15:07:26 -0600

    tab3 add $ format

--- data/processed/biai_strm_demo4.py
+++ data/processed/biai_strm_demo4.py
@@ -419,6 +419,7 @@ with tab3:
         df_avg_cost_speed= df.groupby("Speed_Bin")["Estimated Total Comprehensive Cost"].mean().reset_index()
         fig_avg_cost_speed= px.bar(df_avg_cost_speed, x="Speed_Bin", y="Estimated Total Comprehensive Cost", color="Estimated Total Comprehensive Cost",
             title= "Average Estimated Cost by Speed Bin",labels={"Estimated Total Comprehensive Cost": "Average Estimated Cost", "Speed_Bin": "Speed Limit (mph)"}, text_auto=".2s")
+        fig_avg_cost_speed.update_layout(yaxis_tickprefix='$')
         fig_avg_cost_speed.update_traces(marker_color='#5236ab')
         st.plotly_chart(fig_avg_cost_speed)  
 
