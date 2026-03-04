cdcc320cf274add13f0e8255e3439b27bcff7077
emily-wilkins-owen <emily.wilkins@cgi.com>
2026-03-04 14:46:22 -0600

    tab 4 update heatmap labels

--- data/processed/biai_strm_demo4.py
+++ data/processed/biai_strm_demo4.py
@@ -477,9 +477,19 @@ with tab4:
         y="DAY_NAME",
         z="Count",
         title= "Number of Crashes by Day and Hour",
+        labels={"DAY_NAME": "Day", "HOUR": "Hour", "Count": "Number of Crashes"},
         color_continuous_scale="Purples",
         category_orders={"DAY_NAME": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]},
     )
+    tickvals=[0, 5, 10, 15, 20],
+    ticktext=['12 AM', '5 AM', '10 AM', '3 PM', '8 PM']
+    fig_heat.update_layout(
+    xaxis = dict(
+        tickmode = 'array',
+        tickvals = tick_vals,
+        ticktext = tick_text
+        )
+    )    
     st.plotly_chart(fig_heat, use_container_width=True)
 
     #Average cost by speed bin
